0ca0e3deabcd8dea11a8ddb33d48c7b79b43bcfe
David Corticchiato <david.corticchiato@gmail.com>
2017-03-13 16:38:33 +0100

    build for gh-pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import './index.css';
-
-ReactDOM.render((
-  <h1>
-    ready to <span className="go">go</span>.
-  </h1>
-), document.getElementById('root'));
